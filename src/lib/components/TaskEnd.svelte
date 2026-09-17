5b525e3fc905de76894cf2bbeae2c6f3c4d63958
Abolfazl-Edalati <a.g.e.tech@outlook.com>
2026-09-17 19:44:00 +0330

    learn nested Svelte components

---
+++ src/lib/components/TaskEnd.svelte
@@ -0,0 +1,4 @@
+<div class="mt-2 mb-10">
+	<div>End of task</div>
+	<hr />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,10 @@
-<p>i don't know how to direct a user to another link yet.</p>
+<script lang="ts">
+	import Task1 from '$lib/components/Task1.svelte';
+	import Task2 from '$lib/components/Task2.svelte';
+	import TaskEnd from '$lib/components/TaskEnd.svelte';
+</script>
+
+<Task1 />
+<TaskEnd />
+<Task2 />
+<TaskEnd />
