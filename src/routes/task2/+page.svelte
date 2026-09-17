fa33e7ed9d5b14f38b93e21c6e5fbc1abf4fa93b
Abolfazl-Edalati <a.g.e.tech@outlook.com>
2026-09-17 19:29:38 +0330

    learn Svelte styling and class directives

---
+++ src/routes/task2/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	let isOnline = true;
+</script>
+
+<h1 class:online={isOnline} class:offline={!isOnline}>{isOnline ? 'Online' : 'Offline'}</h1>
+
+<style>
+	.online {
+		background-color: rgb(4, 205, 4);
+	}
+	.offline {
+		background-color: gray;
+	}
+</style>
