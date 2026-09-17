71929a5920f790c50ae7a3845602362a2f0be442
Abolfazl-Edalati <a.g.e.tech@outlook.com>
2026-09-17 19:55:36 +0330

    learn rendering raw HTML in Svelte

---
+++ src/lib/components/Task3.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let content = `
+        <h2>Svelte</h2>
+        <p>I'm learning Svelte</p>
+        <ul>
+            <li>Components</li>
+            <li>Reactivity</li>
+            <li>Props</li>
+        </ul>
+    `;
+</script>
+
+{@html content}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
 	import Task1 from '$lib/components/Task1.svelte';
 	import Task2 from '$lib/components/Task2.svelte';
+	import Task3 from '$lib/components/Task3.svelte';
 	import TaskEnd from '$lib/components/TaskEnd.svelte';
 </script>
 
 <Task1 />
 <TaskEnd />
 <Task2 />
+<TaskEnd />
+<h1 class="my-3 font-bold">
+	The third task was to build a component so i just moved all the tasks to components instead of
+	using a route for each one.
+</h1>
+<TaskEnd />
+<Task3 />
+<h1 class="my-3 font-bold">So basicly this task was to render an raw HTML form script</h1>
 <TaskEnd />
