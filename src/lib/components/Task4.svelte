0938e043ccde33209622c9af124baadff8151682
Abolfazl-Edalati <a.g.e.tech@outlook.com>
2026-09-17 20:08:18 +0330

    learn Svelte state with $state

---
+++ src/lib/components/Task4.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	let count = $state(0);
+</script>
+
+<h1>Count: {count}</h1>
+<h2>Status: {count === 0 ? 'Zero' : count > 0 ? 'Positive' : 'Negative'}</h2>
+
+<div class="mx-2"></div>
+
+<button onclick={() => count++} class="m-0 rounded-2xl border p-2">Increase</button>
+<button onclick={() => count--} class="m-0 rounded-2xl border p-2">Decrease</button>
+<button onclick={() => (count = 0)} class="m-0 rounded-2xl border p-2">Reset</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import Task1 from '$lib/components/Task1.svelte';
 	import Task2 from '$lib/components/Task2.svelte';
 	import Task3 from '$lib/components/Task3.svelte';
+	import Task4 from '$lib/components/Task4.svelte';
 	import TaskEnd from '$lib/components/TaskEnd.svelte';
 </script>
 
@@ -16,4 +17,6 @@
 <TaskEnd />
 <Task3 />
 <h1 class="my-3 font-bold">So basicly this task was to render an raw HTML form script</h1>
+<TaskEnd />
+<Task4 />
 <TaskEnd />
