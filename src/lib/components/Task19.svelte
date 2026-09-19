1f891b999ff56523a446514a5b211cbfd5aa0677
Abolfazl-Edalati <a.g.e.tech@outlook.com>
2026-09-19 17:54:12 +0330

    learn svelte action parameters

---
+++ src/lib/components/Task19.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	function highlight(element: HTMLElement, color: string) {
+		element.style.backgroundColor = color;
+	}
+</script>
+
+<p use:highlight={'yellow'}>
+	Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima, eveniet?
+</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,7 @@
 	import Task16 from '$lib/components/Task16.svelte';
 	import Task17 from '$lib/components/Task17.svelte';
 	import Task18 from '$lib/components/Task18.svelte';
+	import Task19 from '$lib/components/Task19.svelte';
 	import Task2 from '$lib/components/Task2.svelte';
 	import Task3 from '$lib/components/Task3.svelte';
 	import Task4 from '$lib/components/Task4.svelte';
@@ -61,4 +62,6 @@
 <Task17 />
 <TaskEnd />
 <Task18 />
+<TaskEnd />
+<Task19 />
 <TaskEnd />
