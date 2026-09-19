3ecdf15001d77f0773d913b34592cb3ae1b23035
Abolfazl-Edalati <a.g.e.tech@outlook.com>
2026-09-19 12:20:53 +0330

    learn svelte bindings with shadcn-svelte form components

--- src/lib/components/Task10.svelte
+++ src/lib/components/Task10.svelte
@@ -1,4 +1,10 @@
 <script lang="ts">
+	import Input from './ui/input/input.svelte';
+	import Checkbox from './ui/checkbox/checkbox.svelte';
+	import Label from './ui/label/label.svelte';
+	import Textarea from './ui/textarea/textarea.svelte';
+	import * as Select from './ui/select';
+
 	interface Country {
 		code: string;
 		name: string;
@@ -6,7 +12,7 @@
 
 	const countries: Country[] = [
 		{ code: 'ir', name: 'Iran' },
-		{ code: 'us', name: 'United Stated' },
+		{ code: 'us', name: 'United States' },
 		{ code: 'jp', name: 'Japan' }
 	];
 
@@ -15,52 +21,65 @@
 	let temprature = $state(43);
 	let newsletter = $state(true);
 	let skills = $state(['CSS']);
-	let country = $state<Country>(countries[0]);
+	let country = $state<Country['code']>(countries[0].code);
+	let bio = $state('');
 </script>
 
-<form action="">
-	<label>
-		Name
-		<input type="text" bind:value={name} />
-	</label>
-
-	<br />
-	<label>
-		Age
-		<input type="number" bind:value={age} />
-	</label>
-
-	<br />
-	<label>
-		Temperature
-		<input type="range" bind:value={temprature} />
-	</label>
-
-	<br />
-	<label>
-		Newsletter
-		<input type="checkbox" bind:checked={newsletter} />
-	</label>
-	<div>
-		<label>
-			CSS
-			<input type="checkbox" bind:group={skills} value="CSS" />
-		</label>
-
-		<label>
-			HTML
-			<input type="checkbox" bind:group={skills} value="HTML" />
-		</label>
-
-		<label>
-			JS
-			<input type="checkbox" bind:group={skills} value="JS" />
-		</label>
+<form action="" class="flex w-fit flex-col gap-4">
+	<div class="flex flex-col gap-2">
+		<Label for="name">Name</Label>
+		<Input id="name" type="text" bind:value={name} class="w-fit" />
 	</div>
-	<br />
-	<select bind:value={country}>
-		{#each countries as c (c.code)}
-			<option value={c}>{c.name}</option>
-		{/each}
-	</select>
+
+	<div class="flex flex-col gap-2">
+		<Label for="age">Age</Label>
+		<Input id="age" type="number" bind:value={age} class="w-fit" />
+	</div>
+
+	<div class="flex flex-col gap-2">
+		<Label for="temperature">Temperature: {temprature}°</Label>
+		<input id="temperature" type="range" bind:value={temprature} class="w-fit" />
+	</div>
+
+	<div class="flex items-center gap-3">
+		<Checkbox id="newsletter" bind:checked={newsletter} />
+		<Label for="newsletter">Newsletter</Label>
+	</div>
+
+	<div class="flex flex-col gap-2">
+		<Label>Skills</Label>
+		<div class="flex gap-4">
+			<div class="flex items-center gap-2">
+				<input id="css" type="checkbox" bind:group={skills} value="CSS" />
+				<Label for="css">CSS</Label>
+			</div>
+			<div class="flex items-center gap-2">
+				<input id="html" type="checkbox" bind:group={skills} value="HTML" />
+				<Label for="html">HTML</Label>
+			</div>
+			<div class="flex items-center gap-2">
+				<input id="js" type="checkbox" bind:group={skills} value="JS" />
+				<Label for="js">JS</Label>
+			</div>
+		</div>
+	</div>
+
+	<div class="flex flex-col gap-2">
+		<Label for="country">Country</Label>
+		<Select.Root type="single" bind:value={country}>
+			<Select.Trigger id="country" class="w-fit">
+				{countries.find((c) => c.code === country)?.name}
+			</Select.Trigger>
+			<Select.Content>
+				{#each countries as c (c.code)}
+					<Select.Item value={c.code} label={c.name}>{c.name}</Select.Item>
+				{/each}
+			</Select.Content>
+		</Select.Root>
+	</div>
+
+	<div class="flex flex-col gap-2">
+		<Label for="bio">Bio</Label>
+		<Textarea id="bio" bind:value={bio} />
+	</div>
 </form>
