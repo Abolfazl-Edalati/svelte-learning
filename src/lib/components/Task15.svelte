7f6ddd0bcd010187ea4c6e5ad7b46b252d5d3030
Abolfazl-Edalati <a.g.e.tech@outlook.com>
2026-09-19 16:02:11 +0330

    learn svelte key blocks

---
+++ src/lib/components/Task15.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+	import Button from './ui/button/button.svelte';
+
+	let count = $state(0);
+</script>
+
+{#key count}
+	<span transition:fade={{ duration: 500 }}>{count}</span>
+{/key}
+
+<Button onclick={() => count++}>Next</Button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
 	import Task12 from '$lib/components/Task12.svelte';
 	import Task13 from '$lib/components/Task13.svelte';
 	import Task14 from '$lib/components/Task14.svelte';
+	import Task15 from '$lib/components/Task15.svelte';
 	import Task2 from '$lib/components/Task2.svelte';
 	import Task3 from '$lib/components/Task3.svelte';
 	import Task4 from '$lib/components/Task4.svelte';
@@ -49,4 +50,6 @@
 <Task13 />
 <TaskEnd />
 <Task14 />
+<TaskEnd />
+<Task15 />
 <TaskEnd />
