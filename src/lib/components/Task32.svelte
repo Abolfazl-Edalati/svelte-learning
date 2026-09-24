ffe26b3f692553dda9ab8ebbea2b0dfe79407c6f
Abolfazl-Edalati <a.g.e.tech@outlook.com>
2026-09-24 14:14:11 +0330

    learn svelte capture event handler

---
+++ src/lib/components/Task32.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	interface Acc {
+		title: string;
+		desc: string;
+	}
+
+	let title = $state('');
+	let desc = $state('');
+
+	const accordion = $state<Acc[]>([]);
+</script>
+
+<input type="text" placeholder="..." bind:value={title} />
+<textarea name="" id="" bind:value={desc}>Description</textarea>
+<button onclick={() => accordion.push({ title, desc })}>Add</button>
+
+<div>
+	{#each accordion as ac (ac.title)}
+		<div>
+			<span>{ac.title}</span>
+			<p>{ac.desc}</p>
+		</div>
+	{/each}
+</div>
